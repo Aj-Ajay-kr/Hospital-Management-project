12e1f64803a0b50fd26c0fcc26fcc8813a0039d1
Ajay kumar yadav <106795599+Aj-Ajay-kr@users.noreply.github.com>
2023-04-30 21:54:32 +0530

    Hospital Mangment

---
+++ CreatFile.java
@@ -0,0 +1,24 @@
+import .io.*;
+class CreatFile
+{
+	public static void main(String[] args) 
+	{
+		File f=new File("C:\\Users\\yadaw\\Desktop\\new\\ajay.txt");
+	     try 
+	     {
+            if(f.CreatNewFile())
+		      {
+			     System.out.print("file successfully createted......!");
+		      }
+		     else 
+		      {
+			    System.out.print("file Already Exit......!");
+		      }
+	     }
+	     catch(IOException i)
+	     {
+             System.out.print("Exception Handaled......!");
+	     }
+
+	}
+}
